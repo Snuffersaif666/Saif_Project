726cb36822f5792d8e71c897d2ae6063fd068b1b
saifulislam <saifulislam@snuffersaif>
2019-11-18 16:38:05 -0500

    firstCommit

---
+++ gitProject/src/gitProject/Welcome.java
@@ -0,0 +1,13 @@
+package gitProject;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String h = "Hello world";
+		System.out.println(h);
+
+	}
+
+}
